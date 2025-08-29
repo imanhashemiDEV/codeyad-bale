730c2bb4794c36f038621fd8b54a7fa810e930d2
iman hashemi <hashemi.iman@gmail.com>
2025-08-29 22:09:19 +0330

    shop list

--- app/Http/Controllers/Admin/BaleController.php
+++ app/Http/Controllers/Admin/BaleController.php
@@ -26,10 +26,11 @@ class BaleController extends Controller
         }
 
         if (isset($data['callback_query'])) {
-            Log::info($data['callback_query']['data']);
+           // Log::info($data['callback_query']['data']);
             match ($data['callback_query']['data']) {
                 '/products' => $this->getProductsList($data['callback_query']['chat_instance']),
-                $data['callback_query']['data'] => $this->addToCart($data['callback_query']['chat_instance'], $data['callback_query']['data'])
+                '/cart' => $this->getUserCart($data['callback_query']['chat_instance']),
+                $data['callback_query']['data'] => $this->handleCallBack($data['callback_query']['chat_instance'], $data['callback_query']['data'])
                 // $data['callback_query']['data'] => BaleBot::sendMessage($data['callback_query']['chat_instance'], $data['callback_query']['data']),
             };
         }
@@ -59,7 +60,7 @@ class BaleController extends Controller
 
         $buttons = [];
         foreach ($products as $product) {
-            $buttons[][] = ['text' => $product->name . ' - ' . $product->price .' '.'تومان', 'callback_data' => "product/$product->id"];
+            $buttons[][] = ['text' => $product->name . ' - ' . $product->price .' '.'تومان', 'callback_data' => "/product/$product->id"];
         }
 
         Http::post('https://tapi.bale.ai/2082724310:dLjGsp9qoJi85PEWr3vc9zL3xG9c1aofrFVrTD6F/sendMessage', [
@@ -69,6 +70,17 @@ class BaleController extends Controller
                 'inline_keyboard' => $buttons
             ])
         ]);
+    }
+
+    public function handleCallBack($chat_id , $data)
+    {
+        $array = explode('/',$data);
+        $key = $array[1];
+        match ($key) {
+            'product' =>  $this->addToCart($chat_id , $data),
+            'add' =>  $this->addToCart($chat_id , $data),
+            'remove' =>  $this->removeFromCart($chat_id , $data),
+        };
     }
 
     public function addToCart($chat_id , $data)
@@ -89,6 +101,36 @@ class BaleController extends Controller
     BaleBot::sendMessage($chat_id,'محصول به سبد خرید اضافه شد');
 
 }
+
+    public function removeFromCart($chat_id , $data)
+    {
+        BaleBot::sendMessage($chat_id,'محصول از سبد خرید کم شد');
+   }
+
+    public function getUserCart($chat_id)
+    {
+
+        $carts = Cart::query()->where('bale_user_id',$chat_id)->get();
+
+        $total_price = 0;
+        $buttons = [];
+        foreach ($carts as $cart) {
+            $buttons[][] = ['text' => $cart->product->name . ' - ' . ($cart->product->price * $cart->count ) .' '.'تومان'];
+            $buttons[] = [['text' => 'اضافه', 'callback_data' => "/add/$cart->product->id"],['text' => 'کم', 'callback_data' => "/remove/$cart->product->id"]];
+            $total_price += $cart->product->price * $cart->count;
+        }
+
+        $buttons[][] = ['text' => 'قیمت کل:' . $total_price .' '.'تومان'];
+        $buttons[][] = ['text' => 'پرداخت', 'callback_data' => "/pay"];
+
+        Http::post('https://tapi.bale.ai/2082724310:dLjGsp9qoJi85PEWr3vc9zL3xG9c1aofrFVrTD6F/sendMessage', [
+            'chat_id' => $chat_id,
+            'text' => 'لیست سبد خرید شما',
+            'reply_markup' => json_encode([
+                'inline_keyboard' => $buttons
+            ])
+        ]);
+    }
 
 
     public function sendMessage(Request $request)
